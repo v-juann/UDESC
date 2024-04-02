e2fc09cece58a9d10f92e8f41a0aa626e3d5bbec
v-juann <102606376+v-juann@users.noreply.github.com>
2024-04-01 21:42:09 -0300

    Atualização

---
+++ LPG/1.5.1/7.c
@@ -0,0 +1,3 @@
+/*Escreva um algoritmo que determine o valor aproximado do seno de x com base na série abaixo. O
+número de termos da série bem como o valor de x são determinados pelo usuário. Nesta série, o
+primeiro é denominado termo 0 (termo zero).*/
