95f2dd0d2b877cb266777e80f4a5d1f0a17a6bda
v-juann <102606376+v-juann@users.noreply.github.com>
2024-08-14 21:51:12 -0300

    Update 5.c

--- EDA/5.c
+++ EDA/5.c
@@ -3,3 +3,17 @@ desta técnica é representar caracteres repetidos sucessivamente com um contado
 seguido pelo caractere. Dada uma string, retorne o texto resultante da aplicação da
 técnica RLE.
 ○ Exemplo, dada a string "AAAABBBCCDAA", a saída compactada deve ser "4A3B2C1D2A"*/
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char string[10];
+
+    printf("Digite uma cadeia de até 10 caracteres:\n");
+    scanf("%s",string);
+
+    for(int i = 1; i < strlen(string); i++){
+        if(string[i] == string[i])
+    }
+    return 0;
+}
