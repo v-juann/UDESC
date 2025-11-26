8cceef55ccf00cccface160a0625b42594eb3809
v-juann <102606376+v-juann@users.noreply.github.com>
2025-11-26 17:21:44 -0300

    Update TrabalhoSDI.java

--- SDI/Trabalho Final SDI/TrabalhoSDI.java
+++ SDI/Trabalho Final SDI/TrabalhoSDI.java
@@ -1,70 +1,102 @@
-/*Implemente um programa para criptografia e descriptografia de mensagens utilizando o
-algoritmo RSA.
-Você pode utilizar materiais de aula para auxílio, incluindo o Capítulo 10 do livro 
-"Criptografia e Segurança de Redes: Princípios e Práticas" de William Stallings 2006.*/
 import java.util.Scanner;
 import java.util.Random;
+import java.math.BigInteger;
 
 public class TrabalhoSDI{
     public static void main(String[] args){
+        Random rnd = new Random();
+
+        // Ajustar tamanho dos primos para reduzir o tempo de execução
+        int bitLength = 12;
+
         try (Scanner input = new Scanner(System.in)) {
+
+            // Obtém a string de entrada do usuário
             System.out.println("Digite uma string:\n");
             String entrada = input.nextLine();
             
-            conversaoAscii(entrada);
-            cripto(entrada);
+            // Conversão da entrada em código ASCII
+            BigInteger codigoAscii = new BigInteger(conversaoAscii(entrada));
+
+            // Inicia os primos p, q e o módulo n
+            BigInteger n = BigInteger.ZERO, p = BigInteger.ZERO, q = BigInteger.ZERO;
+
+            // Garantindo que n é maior que o código ASCII
+            while(n.compareTo(codigoAscii) < 0){
+                p = BigInteger.probablePrime(bitLength, rnd);
+                q = BigInteger.probablePrime(bitLength, rnd);
+                n = p.multiply(q);
+            }
+
+            // Cálculo da função totiente, expoentes público e privado
+            BigInteger phi = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
+            BigInteger publicExpoent = publicExpoent(phi);
+            BigInteger privateExpoent = privateExpoent(publicExpoent, phi);
+            
+            // Criptografia e Descriptografia
+            BigInteger msgCriptografada = cripto(n, publicExpoent, privateExpoent, codigoAscii);
+            String msgDescriptografada = desconversaoAscii(descripto(msgCriptografada, privateExpoent, n));
+
+            // Saída
+            System.out.println(
+            "\nEntrada: " + entrada + 
+            "\nCodigo ASCII: " + codigoAscii +
+            "\nCriptografada: " + msgCriptografada + 
+            "\nChave Publica: (" + n + ", " + publicExpoent + ")" +
+            "\nChave Privada: (" + n + ", " + privateExpoent + ")" +
+            "\nDescriptografada (ASCII): " + descripto(msgCriptografada, privateExpoent, n) + 
+            "\nDescriptografada (Texto): " + msgDescriptografada);
         }
-    };
-
-    public static void conversaoAscii(String entrada){
-        for( int i = 0; i < entrada.length(); i++ ){
-            int caractere = entrada.charAt(i);
-            System.out.printf("%d ", caractere);
+        catch (Exception e) {
+            System.out.println("Erro ao obter entrada: " + e.getMessage());
         }
-    };
-
-    public static void cripto(String entrada){
-        int prime1 = primeGen(9999);
-        int prime2 = primeGen(9999);
-        int n = prime1 * prime2;
-        int totient = (prime1-1) * (prime2-1);
-
-        int publicExpoent = primeGen(totient);
-
-    };
-
-    public static int primeGen(int bound){
-        Random rand = new Random();
-        int z = rand.nextInt(bound) + 2;
-        while (!isPrime(z)) {
-            z = rand.nextInt(bound) + 2;
-        }
-        return z;
     };
     
-    public static boolean isPrime(int num) {
-        if (num <= 1) {
-            return false;
+    public static String conversaoAscii(String entrada){
+        String codigo = new String();
+        for(int i = 0; i < entrada.length(); i++){
+            int caractere = entrada.charAt(i);
+            codigo += caractere;
         }
-        if (num <= 3) {
-            return true;
-        }
-        if (num % 2 == 0 || num % 3 == 0) {
-            return false;
-        }
-        for (int i = 5; i * i <= num; i = i + 6) {
-            if (num % i == 0 || num % (i + 2) == 0) {
-                return false;
+        return codigo;
+    };
+    
+    public static String desconversaoAscii(BigInteger entrada){
+        String codAscii = entrada.toString();
+        String stringConvertida = new String();
+
+        for(int i = 0; i <= codAscii.length()-2; i+=2){
+            int caractere = Integer.parseInt(codAscii.substring(i, i+2));
+            if(caractere < 32){
+                caractere = Integer.parseInt(codAscii.substring(i, i+3));
+                i++;
             }
+            stringConvertida += (char) caractere;
         }
-        return true;
+
+        return stringConvertida;
     };
 
-    public static int privateExpoent(int e, int publicExpoent){
-        int a = 1;
-        while ( (a * e)  % publicExpoent)!= 1){
-            a++;
-        };
-        return a;
+    public static BigInteger cripto(BigInteger n, BigInteger publicExpoent, BigInteger privateExpoent, BigInteger entrada){        
+        BigInteger C = entrada.modPow(publicExpoent, n);        
+        return C;
+    };
+
+    public static BigInteger descripto(BigInteger C, BigInteger privateExpoent, BigInteger n){
+        BigInteger M = C.modPow(privateExpoent, n);
+        return M;
+    };
+ 
+    public static BigInteger privateExpoent(BigInteger publicExpoent, BigInteger phi){
+        BigInteger d = publicExpoent.modInverse(phi);
+        return d;
+    };
+    
+    public static BigInteger publicExpoent(BigInteger phi){
+        BigInteger e = BigInteger.valueOf(2);
+        while(e.gcd(phi).intValue() != 1){
+            e = e.add(BigInteger.ONE);
+        }
+        return e;
     };
 };
